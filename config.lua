027f55ed57b17d923f4269046d5b2f1aa4efccfa
Brodino <119953295+Brodino96@users.noreply.github.com>
2023-11-15 22:23:20 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,9 +12,10 @@ Config.price = {
 Config.ignoreJob = true                         -- If "true" will ignore any job and go straight to Config.defaultHome
 Config.defaultHome = { x = 1975.47, y = 3820.39, z = 33.44, heading = 138.89 }
 
+
 Config.homes = {                                -- Orientation doesn't do anything for now
-    { jobname = "rac", x = -1627.51, y = -1035.73, z = 13.13, heading = 48.18 },        -- Compares your current job with everyone of these, when it finds a match will teleport the player to those coords
-    { jobname = "noa", x = 481.63, y = -2962.02, z = 6.04, heading = 175.74 },
+    { jobname = "police", x = 451.34, y = -923.19, z = 28.46, heading = 179.38 },        -- Compares your current job with everyone of these, when it finds a match will teleport the player to those coords
+    { jobname = "ems", x = 212.06, y = 564.632, z = 43.87, heading = 255.58 },
 }
 
 Config.MinusOne = true                          -- If you use txadmin or esx /coords keep this on true, basically it does (z - 1) because i need feet position and some methodes to get coords give you head coords
